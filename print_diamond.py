302f3a7a5174ab208abbcd5d5337c54545e04950
aieckxis <aleckxiskate@gmail.com>
2023-09-30 14:27:11 +0800

    printed the bottom half (didn't work)

--- print_diamond.py
+++ print_diamond.py
@@ -6,8 +6,13 @@ def print_diamond():
     # Print the top half of the diamond
     for diamond in range(1, n, 2):
         print(" " * ((n - diamond) // 2) + "*" * diamond)
-# Print the middle row with all asterisks
-# Print the bottom half of the diamond
+
+    # Print the middle row with all asterisks
+
+    # Print the bottom half of the diamond
+    for diamond in range(n - 2, n, -2):
+        print(" " * ((n - diamond) // 2) + "*" * diamond)
+
 # Test the function with n = 5
 n = 5
 print_diamond()
