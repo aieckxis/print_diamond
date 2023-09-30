4a39ac34d5afb5be61fdd704bb51f17282e1a306
aieckxis <aleckxiskate@gmail.com>
2023-09-30 14:08:31 +0800

    defined the function

--- print_diamond.py
+++ print_diamond.py
@@ -1,4 +1,8 @@
 # Define a function to print a diamond pattern with asterisks
+def print_diamond:
+    if n % 2 == 0:
+        return "Please provide an odd integer."
+
 # Print the top half of the diamond
 # Print the middle row with all asterisks
 # Print the bottom half of the diamond
