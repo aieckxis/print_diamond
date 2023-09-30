81bfafdd71de33c129789c92140c2417efb260ec
aieckxis <aleckxiskate@gmail.com>
2023-09-30 14:03:25 +0800

    pseudocode

---
+++ print_diamond.py
@@ -0,0 +1,5 @@
+# Define a function to print a diamond pattern with asterisks
+# Print the top half of the diamond
+# Print the middle row with all asterisks
+# Print the bottom half of the diamond
+# Test the function with n = 5
