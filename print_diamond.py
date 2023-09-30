7b73dbcc4675b54dddbeb6524446186979d379eb
aieckxis <aleckxiskate@gmail.com>
2023-09-30 14:19:28 +0800

    printed the top half of the diamond and tested the function

--- print_diamond.py
+++ print_diamond.py
@@ -3,7 +3,11 @@ def print_diamond():
     if n % 2 == 0:
         return "Please provide an odd integer."
 
-# Print the top half of the diamond
+    # Print the top half of the diamond
+    for diamond in range(1, n, 2):
+        print(" " * ((n - diamond) // 2) + "*" * diamond)
 # Print the middle row with all asterisks
 # Print the bottom half of the diamond
 # Test the function with n = 5
+n = 5
+print_diamond()
